145d948c33e357b2c4d9ce8f950e4f715f75c50c
ehmidan <ehmidan.soulayman.solicode@gmail.com>
2021-06-30 00:31:29 +0100

    added a sideBar

---
+++ project_Fin_De_Tude/index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="./main.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="wrapper">
+
+<nav id="sidebar">
+   <div class="sidebar-header">
+   <h3>blblala</h3>
+   </div>
+
+    <ul class="lisst-unstyled components">
+
+      <li class="active">
+        <a href="#">Home</a>
+      </li>
+
+      <li>
+        <a href="#">about</a>
+      </li>
+
+        <li>
+        <a  href="#">pages</a>
+       
+        </li>
+
+
+          <li>
+          <a href="#">contact us</a>
+          </li>
+
+    
+    </ul>
+  
+</nav>
+
+
+<div id="content">
+  
+<nav  class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container-fluid">
+<button type="button" id="sidebarCollapse" data-toggle="collapse" class="btn btn-info">
+<span >toggle</span>
+</button>
+</div>
+</nav>
+
+<br><br>
+<h2>hello guys this is my pft</h2>
+<p>djhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyy
+</p>
+<p>djhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyy
+</p>
+<p>djhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyy
+</p>
+
+<h3>gfsdjhgshdd  </h3>
+<p>djhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyhhhhhhhhfg
+ffffffffffffffffffffffffffffffffffffff
+fffffffffffffffffffffffffffffffffffffffff
+jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+yyyyyyyyyyyyyyyy
+</p>
+
+ 
+
+
+
+</div>
+</div>
+    
+    <script src="./bootstrap/js/popper.min.js"></script>
+    <script src="./bootstrap/js/jquery-3.5.1.min.js"></script>
+    <script src="./bootstrap/js/bootstrap.js"></script>
+
+    <script >
+       $(document).ready(function(){
+       $("#sidebarCollapse").on('click',function(){
+       $("#sidebar").toggleClass('active');
+         });
+       });
+    </script>
+</body>
+</html>
